393f12f68d01db3a174eb9da80c712e24e7a86eb
KingOfTurkey38 <ozomer792@gmail.com>
2019-12-29 23:55:33 +0100

    Added declare strict types and removed license ( phpstorm added it automaticly lol )

--- src/Twisted/BetterVoting/BetterVoting.php
+++ src/Twisted/BetterVoting/BetterVoting.php
@@ -1,23 +1,4 @@
 <?php
-/**
- *      ___        __  __              ____  ______
- *     /   | ___  / /_/ /_  ___  _____/ __ \/ ____/
- *    / /| |/ _ \/ __/ __ \/ _ \/ ___/ /_/ / __/
- *   / ___ /  __/ /_/ / / /  __/ /  / ____/ /___
- *  /_/  |_\___/\__/_/ /_/\___/_/  /_/   /_____/
- *
- * Copyright (c) KingOfTurkey38 2019.
- * Last Modified 29/12/19 23:53
- *
- * This plugin is developed and maintained by KingOfTurkey38.
- *
- * This plugin is distributed under a commercial license and you are only allowed to use it in the way and
- * extent and under conditions explicitly agreed on with KingOfTurkey38 in writing.
- *
- * By the license, you're not allowed to remove this documented snippet.
- *
- */
-
 declare(strict_types=1);
 
 namespace Twisted\BetterVoting;
